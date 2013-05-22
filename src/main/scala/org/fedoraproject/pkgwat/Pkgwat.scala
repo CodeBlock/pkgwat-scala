2e55c617f2c599c29319b7af7dafc69597824c55
Ricky Elrod <ricky@elrod.me>
2013-05-22 15:50:39 -0400

    Make the JSON Search transformer reusable.

--- src/main/scala/org/fedoraproject/pkgwat/Pkgwat.scala
+++ src/main/scala/org/fedoraproject/pkgwat/Pkgwat.scala
@@ -41,7 +41,28 @@ class Pkgwat(baseURL: String) {
       URLEncoder.encode(json, "utf8")).mkString("/")
   }
 
-  /** Returns a [[SearchResults]] after searching the Fedora Packages app for a package.
+  /** Returns a [[SearchResults]] with extracted JSON search results.
+    *
+    * @param content The raw JSON string returned from the server.
+    */
+  private def extractSearchJSON(content: String): SearchResults = {
+    // Strip tags and slurp into lift-json's magic.
+    val json = parse(content.replaceAll("""<\/?.*?>""", ""))
+
+    // Transform so that we match our case classes above.
+    json.transform {
+      case JField("upstream_url", x) => JField("upstreamURL", x)
+      case JField("sub_pkgs", x) => JField("subPackages", x)
+      case JField("devel_owner", x) => JField("develOwner", x)
+      case JField("visible_rows", x) => JField("visibleRows", x)
+      case JField("start_row", x) => JField("startRow", x)
+      case JField("rows_per_page", x) => JField("rowsPerPage", x)
+      case JField("total_rows", x) => JField("totalRows", x)
+    }.extract[SearchResults]
+  }
+
+
+  /** Returns a [[Future[SearchResults]]] after searching for a package.
     *
     * @param query The package to search for.
     * @param startRow The result row number to start at.
@@ -59,27 +80,13 @@ class Pkgwat(baseURL: String) {
 
     for (result <- Http(url(jsonURL) OK as.String).either) yield {
       result match {
-        case Right(content) => {
-          // Strip tags and slurp into lift-json's magic.
-          val json = parse(content.replaceAll("""<\/?.*?>""", ""))
-
-          // Transform so that we match our case classes above.
-          json.transform {
-            case JField("upstream_url", x) => JField("upstreamURL", x)
-            case JField("sub_pkgs", x) => JField("subPackages", x)
-            case JField("devel_owner", x) => JField("develOwner", x)
-            case JField("visible_rows", x) => JField("visibleRows", x)
-            case JField("start_row", x) => JField("startRow", x)
-            case JField("rows_per_page", x) => JField("rowsPerPage", x)
-            case JField("total_rows", x) => JField("totalRows", x)
-          }.extract[SearchResults]
-        }
+        case Right(content) => extractSearchJSON(content)
         case Left(error) => throw error
       }
     }
   }
 
-  /** Returns an [[Option[Package]]] after searching Fedora Packages for it.
+  /** Returns a [[Future[Option[Package]]]] after searching Fedora Packages for it.
     *
     * This method **blocks** for now, because we need to traverse the results
     * to look for subpackages. However, we can traverse in the future, so we
@@ -102,20 +109,7 @@ class Pkgwat(baseURL: String) {
     for (result <- Http(url(jsonURL) OK as.String).either) yield {
       result match {
         case Right(content) => {
-          // Strip tags and slurp into lift-json's magic.
-          val json = parse(content.replaceAll("""<\/?.*?>""", ""))
-
-          // Transform so that we match our case classes above.
-          val results = json.transform {
-            case JField("upstream_url", x) => JField("upstreamURL", x)
-            case JField("sub_pkgs", x) => JField("subPackages", x)
-            case JField("devel_owner", x) => JField("develOwner", x)
-            case JField("visible_rows", x) => JField("visibleRows", x)
-            case JField("start_row", x) => JField("startRow", x)
-            case JField("rows_per_page", x) => JField("rowsPerPage", x)
-            case JField("total_rows", x) => JField("totalRows", x)
-          }.extract[SearchResults]
-
+          val results = extractSearchJSON(content)
           val pkg = (results.rows.map(_.subPackages).flatten.filter(_.name == query) ++
             results.rows.filter(_.name == query))
           if (pkg.length == 1) Some(pkg.head) else None
